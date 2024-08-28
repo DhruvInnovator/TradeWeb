83fa9cded5d106ee4d5e5c206942ea01aff45289
DhRuVUser04 <dhruvnegi2040@gmail.com>
2024-08-28 21:20:50 +0530

    feat: Add robots.txt and sitemap.xml generation
    This commit adds the functionality to generate the `robots.txt` and `sitemap.xml` files for the website. The `robots.ts` file defines the rules for web crawlers, allowing access to the root URL and disallowing access to the `/private/` path. The `sitemap.ts` file defines the URLs to include in the sitemap, including the homepage, about page, and a blog URL.
    
    The changes also include recent user commits related to error fixes, login session, and icon updates.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = 'https://www.rashi-trading.in'
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://acme.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
